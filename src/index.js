5ba7962bcfa1cc8bcd3b8574ecdb22eba88d7a79
divine dela <divinedela.dd@gmail.com>
2020-01-18 00:34:34 +0000

    render a simple element in the DOM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Hello from "./hello";
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<Hello/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = <h1>Hello Day4</h1>
+ReactDom.render(element, document.getElementById('root'));
