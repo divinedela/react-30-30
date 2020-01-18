ef7b1920ca7fb1327b4873a10a5ec6bb4e599b24
divine dela <divinedela.dd@gmail.com>
2020-01-18 00:42:20 +0000

    updating a rendered react element

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const element = <h1>Hello Day4</h1>
-ReactDom.render(element, document.getElementById('root'));
+
+
+//Updating the Rendered Element
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello Day4</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    ReactDom.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
